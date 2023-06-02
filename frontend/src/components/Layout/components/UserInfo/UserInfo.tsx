1b20a93dae7bb5c406aca34378d0e20cb6ffaf03
DimaSadovsky21 <dimasadovsky21@gmail.com>
2023-06-02 11:59:22 +0300

    chore: delete ActivityInfo to UserInfo

--- frontend/src/components/Layout/components/UserInfo/UserInfo.tsx
+++ frontend/src/components/Layout/components/UserInfo/UserInfo.tsx
@@ -3,7 +3,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { IUserResponse } from "types/user";
 import { QUERY_KEYS } from "api/constants";
 
-import { ActivityInfoWrapper, UserAvatarWrapper, UserInfoWrapper, Username } from "./styles";
+import { UserAvatarWrapper, UserInfoWrapper, Username } from "./styles";
 import { Avatar } from "./components/Avatar";
 import { Status } from "./components/Status";
 
@@ -22,9 +22,7 @@ const UserInfo = () => {
         {username && <Username>@{username}</Username>}
       </UserAvatarWrapper>
       <Status/>
-      <ActivityInfoWrapper>
-        <p>Likes 25</p> <p>Posts 25</p>
-      </ActivityInfoWrapper>
+      
     </UserInfoWrapper>
   );
 };
--- frontend/src/components/Layout/components/UserInfo/styles.ts
+++ frontend/src/components/Layout/components/UserInfo/styles.ts
@@ -1,31 +1,24 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const UserInfoWrapper = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  width: "30%",
-  height: "100%",
-  gap: "20px",
-  padding: "20px",
-  border: "2px solid var(--gray)",
-  borderRadius: "5px",
-  background: "var(--white)",
+export const UserInfoWrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '30%',
+  height: '100%',
+  gap: '20px',
+  padding: '20px',
+  border: '2px solid var(--gray)',
+  borderRadius: '5px',
+  background: 'var(--white)',
 });
 
-export const UserAvatarWrapper = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
+export const UserAvatarWrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 });
 
-export const Username = styled("p")({
-  color: "var(--dark-blue)",
+export const Username = styled('p')({
+  color: 'var(--dark-blue)',
 });
-
-export const ActivityInfoWrapper = styled("div")({
-  display: "flex",
-  gap: "10px",
-  fontSize: "var(--small-font)",
-  color: "var(--dark-gray)"
-})
